2e672067e06c5ba50995b5f923b0d54a2d7788c3
Ramakrishna Sistla <54027855+SRamakrishna77@users.noreply.github.com>
2020-08-28 19:12:00 +0530

    Add files via upload

---
+++ python_assignment2.1.py
@@ -0,0 +1,8 @@
+### write a python program to reverse a word after accepting the input from the user( No validations are added in the code)
+#Inputword = ineuron
+#output word = norueni
+
+iput = input("Enter the word  : ")
+print(iput)
+r_iput  = str(iput[::-1])
+print(r_iput)
---
+++ python_assignment2.py
@@ -0,0 +1,18 @@
+#Create the below pattern using nested for loop in Python.
+
+for i in range(0, 5):
+    for j in range(0, i + 1):
+        print("*", end=' ')
+    print(" ")
+
+for i in range(5 , 0,-1):
+    for j in range(0, i - 1):
+        print("*", end=' ')
+    print(" ")
+
+
+    
+
+
+
+
